d0ac9b6dad8e5eee53055b15f89323cbb26ecbff
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-01-17 23:47:50 -0700

    Create show method in BulkDiscountsController; Create show view page for bulk_discounts

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,6 +2,10 @@ class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+
+  end
   
   def new
     @merchant = Merchant.find(params[:merchant_id])

09ae4bfc2711738402d3caa7874e24e5de34d9b9
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-01-17 22:07:54 -0700

    Update test for float percentage; Update routes for create route; Update BulkDiscountsController new and update methods and params; Complete US2

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -8,22 +8,16 @@ RSpec.describe 'Bulk Discounts New Page' do
   end
 
   describe 'User Story #2' do
-    it 'where I see a form to add a new bulk discount' do
-      within("#new_discount_form") do
-        expect(page).to have_form('Create Discount')
-      end
-    end
-
     it 'fills in the form with valid data and is redirected back to the bulk discount index' do
       within("#new_discount_form") do
         fill_in :percentage, with: 15
         fill_in :quantity_threshold, with: 5
         click_button 'Create Bulk Discount'
-
-        expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
-        expect(page).to have_content('Percentage: 15')
-        expect(page).to have_content('Quantity Threshold: 5')
       end
+      
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+      expect(page).to have_content('Percentage: 15.0')
+      expect(page).to have_content('Quantity Threshold: 5')
     end
   end
 end
